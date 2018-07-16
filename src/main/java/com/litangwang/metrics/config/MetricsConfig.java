523a10fb67e4257f6abd22dfe41f5917c4d52434
gmbody7 <gmbody7@gmail.com>
2018-07-15 21:41:06 -0700

    add data metrics bean

--- src/main/java/com/litangwang/metrics/config/MetricsConfig.java
+++ src/main/java/com/litangwang/metrics/config/MetricsConfig.java
@@ -1,6 +1,10 @@
 package com.litangwang.metrics.config;
 
 import com.litangwang.metrics.DataMetricsFilter;
+import com.litangwang.metrics.DataMetricsReporter;
+import com.litangwang.metrics.DataMetricsWriter;
+import com.litangwang.metrics.impl.DataMetricsReporterImpl;
+import com.litangwang.metrics.impl.DataMetricsWriterImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -10,5 +14,15 @@ public class MetricsConfig {
     @Bean
     public OncePerRequestFilter dataLogFilter() {
         return new DataMetricsFilter();
+    }
+
+    @Bean
+    public DataMetricsReporter dataMetricsReporter() {
+        return new DataMetricsReporterImpl();
+    }
+
+    @Bean
+    public DataMetricsWriter dataMetricsWriter() {
+        return new DataMetricsWriterImpl();
     }
 }
